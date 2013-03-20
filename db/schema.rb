56f39eedf88b46a9aa1d894374c6729cf9b38883
John Cockrum <johncockrum1@gmail.com>
2013-03-20 07:12:56 -0700

    working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130319152116) do
+ActiveRecord::Schema.define(:version => 20130320055145) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
