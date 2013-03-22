ffeb5c6b5c5c5e6561a6c029e708434544646afc
John Cockrum <johncockrum1@gmail.com>
2013-03-21 22:16:20 -0700

    working on views

--- app/controllers/universes_controller.rb
+++ app/controllers/universes_controller.rb
@@ -1,4 +1,12 @@
 class UniversesController < InheritedResources::Base
   
 
+  def create
+    @universe = Universe.new(params[:universe])
+    @universe.users_id = current_user.id
+    create!
+  end
+
+
+
 end
