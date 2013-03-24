a775c319fbdccbb9b6b9719df461bac947ec62c1
John Cockrum <johncockrum1@gmail.com>
2013-03-24 12:17:53 -0700

    wrapup on universe

--- app/controllers/universes_controller.rb
+++ app/controllers/universes_controller.rb
@@ -1,34 +1,33 @@
 class UniversesController < InheritedResources::Base
-  respond_to :html, :json
-  before_filter :load_sidestack
-  
-  def create
-    @universe = Universe.new(params[:universe])
-    @universe.users_id = current_user.id
-    if @universe.save
-      redirect_to :back, :notice => "Successfully created universe."
-    else
-      render :new
+    respond_to :html, :json
+    before_filter :load_sidestack
+    def create
+        @universe = Universe.new(params[:universe])
+        @universe.users_id = current_user.id
+        if @universe.save
+            redirect_to :back, :notice => "Successfully created universe."
+        else
+            render :new
+        end
     end
-  end
-  
-  def update
-    @universe = Universe.find(params[:id])
-  respond_to do |format|
-    if @universe.update_attributes(params[:universe])
-      format.html { redirect_to(@universe, :notice => 'universe was successfully updated.') }
-      format.json { respond_with_bip(@universe) }
-    else
-      format.html { render :action => "edit" }
-      format.json { respond_with_bip(@universe) }
+
+    def update
+        @universe = Universe.find(params[:id])
+        respond_to do |format|
+            if @universe.update_attributes(params[:universe])
+                format.html { redirect_to(@universe, :notice => 'universe was successfully updated.') }
+                format.json { respond_with_bip(@universe) }
+            else
+                format.html { render :action => "edit" }
+                format.json { respond_with_bip(@universe) }
+            end
+        end
     end
-  end
-  end
-  
-  private  
-  
-  def load_sidestack
-    @users = User.all
-  end
-  
+
+    private
+
+    def load_sidestack
+        @users = User.all
+    end
+
 end
