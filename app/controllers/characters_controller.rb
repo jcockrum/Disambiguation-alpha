8dda337f4180274aa369858a9d66e81ccd64b803
John Cockrum <johncockrum1@gmail.com>
2013-03-31 09:29:01 -0700

    setup characters dashboard

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,7 +4,12 @@ class CharactersController < InheritedResources::Base
     #respond_to :html, :json
     #actions :index, :new, :create, :delete
 
+    def index
+        index! @character = Character.find(params[:character_id]) if params[:character_id]
+    end
+
     protected
+
     def begin_of_association_chain
         @universe
     end
