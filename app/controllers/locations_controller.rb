184847fb303471ab884f6cf47675ec5f7233184c
John Cockrum <johncockrum1@gmail.com>
2013-04-01 01:17:43 -0700

    locaitons dashboard CFN

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,36 @@
 class LocationsController < InheritedResources::Base
 	belongs_to :universe
     respond_to :html, :json
+    
+       actions :index, :new, :create, :update, :destroy
+       
+    def index
+        @universe = Universe.find(params[:universe_id])
+        @locations = @universe.locations
+        @location = Location.find(params[:location_id]) if params[:location_id]
+    end
+
+    def create
+	    super do |format|
+	      format.html { redirect_to universe_locations_url }
+	    end
+	end
+
+    def update
+        @location = Location.find(params[:id])
+        respond_to do |format|
+            if @location.update_attributes(params[:location])
+                format.html { redirect_to(@location, :notice => 'location was successfully updated.') }
+                format.json { respond_with_bip(@location) }
+            else
+                format.html { render :action => "edit" }
+                format.json { respond_with_bip(@location) }
+            end
+        end
+    end
+    
+    
+    
+    
+    
 end
