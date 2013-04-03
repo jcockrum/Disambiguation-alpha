1dea1a2f297b2848e5ae545c843b0eeab91cef63
John Cockrum <johncockrum1@gmail.com>
2013-04-03 02:45:30 -0700

    shallow: storyarcs completed

--- app/controllers/storyarcs_controller.rb
+++ app/controllers/storyarcs_controller.rb
@@ -1,39 +1,44 @@
 class StoryarcsController < InheritedResources::Base
-    belongs_to :universe
-    respond_to :html, :json
-    before_filter :access_externals
-    #TODO: check that :access_externals does not crash on empty set
-
-	def index
-		@storyarcs = @universe.storyarcs		
-    	@storyarc = Storyarc.find(params[:storyarc_id]) if params[:storyarc_id]
-	end
-
-
-    def create
-        super do |format|
-            format.html { redirect_to universe_storyarcs_url }
-        end
+	respond_to :html, :json
+	
+     def index
+        @universe = Universe.find(params[:universe_id])
+        @storyarcs = @universe.storyarcs
     end
 
-    #TODO: storyarc is not updateing
+    def new
+		@universe = Universe.find(params[:universe_id])
+		@storyarc = @universe.storyarc.build
+    end
+
+    def create
+        @universe = Universe.find(params[:universe_id])
+	    @storyarc = @universe.storyarcs.build(params[:storyarc])
+	    if @storyarc.save
+	      flash[:notice] = "Successfully created storyarc."
+	      redirect_to universe_storyarcs_url
+	    else
+	      render :action => 'new'
+	    end
+    end
+
     def update
-        @storyarc = storyarc.find(params[:id])
+        @storyarc = Storyarc.find params[:id]
         respond_to do |format|
             if @storyarc.update_attributes(params[:storyarc])
-                format.html { redirect_to(@storyarc, :notice => 'storyarc was successfully updated.') }
+                format.html { redirect_to(universe_storyarcs_url , :notice => 'storyarc was successfully updated.') }
                 format.json { respond_with_bip(@storyarc) }
             else
                 format.html { render :action => "edit" }
                 format.json { respond_with_bip(@storyarc) }
             end
         end
-    end
+   end
 
-    private
-
-    def access_externals
-        @universe = Universe.find(params[:universe_id])
-        @storyarc = Storyarc.find(:first)
-    end
+    def destroy
+	    @storyarc = Storyarc.find(params[:id])
+	    @storyarc.destroy
+	    flash[:notice] = "Successfully destroyed storyarc."
+	    redirect_to :back
+	end
 end
