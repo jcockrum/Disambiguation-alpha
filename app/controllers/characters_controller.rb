f97d1b92900f98fea104a361c45166d55ca0df45
John Cockrum <johncockrum1@gmail.com>
2013-04-15 14:06:12 -0700

    association adjustment

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,5 +2,17 @@ class CharactersController < InheritedResources::Base
     respond_to :html, :json
     belongs_to :scene
         
- 
+     def show
+        @character = Character.find params[:id]
+        @partible = @character
+        @parts = @partible.parts
+        @part = Part.new
+    end
+    
+    def destroy
+    	@character = Character.find params[:id]
+        @character.destroy
+	    flash[:notice] = "Successfully destroyed scene."
+	    redirect_to :back
+    end
 end
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,5 +2,17 @@ class LocationsController < InheritedResources::Base
     respond_to :html, :json
     belongs_to :scene
         
- 
+     def show
+        @location = Location.find params[:id]
+        @partible = @character
+        @parts = @partible.parts
+        @part = Part.new
+    end
+    
+    def destroy
+    	@location = Location.find params[:id]
+        @location.destroy
+	    flash[:notice] = "Successfully destroyed scene."
+	    redirect_to :back
+    end
 end
