a6ce219b1c339ef56d2efe34bad8445d587ce573
John Cockrum <johncockrum1@gmail.com>
2013-04-22 14:29:15 -0700

    setup layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,16 +7,63 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+// The jQuery UI code weighs 51KB (minified + gzipped) and takes a while to execute, so for production
+// apps it's recommended to only include the modules that your application actually uses. Dependencies
+// are automatically resolved. Simply pick one or more modules from the asset list below.
+//// UI Core ////
+//  require jquery.ui.core
+//  require jquery.ui.widget
+//  require jquery.ui.mouse
+//  require jquery.ui.position
+// You usually do not need to require these directly, as they are pulled in by the other JavaScript
+// modules as needed.
+//// Interactions ////
+//  require jquery.ui.draggable
+//  require jquery.ui.droppable
+//  require jquery.ui.resizable
+//  require jquery.ui.selectable
+//  require jquery.ui.sortable
+// For jquery.ui.resizable and jquery.ui.selectable, remember to require their matching CSS files
+// in your application.css as well.
+//// Widgets ////
+// require jquery.ui.accordion
+// require jquery.ui.autocomplete
+// require jquery.ui.button
+// require jquery.ui.datepicker
+// require jquery.ui.dialog
+// require jquery.ui.menu
+// require jquery.ui.progressbar
+// require jquery.ui.slider
+// require jquery.ui.spinner
+// require jquery.ui.tabs
+// require jquery.ui.tooltip
+// For all of these, remember to require their matching CSS files in your application.css as well.
+
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//
+
+//// Includes ////
 //= require jquery
-//= require jquery-ui
-//= require best_in_place
+//= require jquery.ui.core
+//= require jquery.ui.widget
+//= require jquery.ui.mouse
+//= require jquery.ui.position
 //= require jquery_ujs
+//= require best_in_place
+//= require cocoon
+//= addon
 //= require_tree .
 
-$(document).ready(function() {
-  /* Activating Best In Place */
-  jQuery(".best_in_place").best_in_place();
-});
+$(document).ready(function()
+{
+    /* Activating Best In Place */
+    jQuery(".best_in_place").best_in_place();
+    /* page custom */
+    $('ul.sf-menu').superfish();
+    $('#wrapper .border-top').pngie();
+    $('#container .ft').pngie();
+    $('#container .hd').pngie();
+    $('#container .bd').pngie();
+    $('#wrapper .overlay').pngie();
+}); 
